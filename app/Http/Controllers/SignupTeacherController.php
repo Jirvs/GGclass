d9022ae52721ed628d5fd4e9cc5a0a729bf23f39
GG Automation <delunakathleen3@gmail.com>
2024-09-11 00:20:55 +0800

    initial commit

---
+++ app/Http/Controllers/SignupTeacherController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class SignupTeacherController extends Controller
+{
+    public function handleSignup(Request $request)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'id_number' => 'required|string|unique:users,id_number',
+            'birthday' => 'required|date',
+            'gender' => 'required|in:male,female,other',
+        ]);
+        
+        if(!$validatedData) {
+            return redirect()->back()->with('error', $validatedData);
+        }
+
+        // Insert the validated data into the teachers table
+        User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'id_number' => $request->id_number,
+            'birthday' => $request->birthday,
+            'gender' => $request->gender,
+            'user_type' => 'teacher',
+            'is_approve' => 0,
+        ]);
+
+        // Redirect or send a response after successful sign-up
+        return redirect('/auth.view')->with('message', 'You have signup already, please wait for the admin to approve your account.');
+    }
+}
---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Course;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Course::insert([
+        ['course_name' => 'Computer Science'],
+        ['course_name' => 'Information Technology'],
+        ['course_name' => 'Information System']
+        ]);
+    }
+}
